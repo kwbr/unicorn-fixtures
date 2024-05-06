aad4b4030feecf2ef662bf4066680aa21b55a520
Kai Weber <kai.weber@glorybox.de>
2024-05-06 13:21:57 +1000

    Update fetch-fixture.sh

--- fetch-fixture.sh
+++ fetch-fixture.sh
@@ -1,20 +1,18 @@
 #!/usr/bin/env bash
 
-url='https://mc-api.dribl.com/api/fixtures?date_range=default&season=3vmZpnGKq5&competition=R3NPZlPdr4&league=8nm7kPnLmO&club=RwNl4geNjr&tenant=kbam1QjmwX'
-
-curl "$url" \
-    --compressed \
-    --header 'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:124.0) Gecko/20100101 Firefox/124.0' \
-    --header 'Accept: application/json' \
-    --header 'Accept-Language: en-US,en;q=0.5' \
-    --header 'Accept-Encoding: gzip, deflate, br' \
-    --header 'X-Requested-With: XMLHttpRequest' \
-    --header 'Origin: https://mwfa.dribl.com' \
-    --header 'DNT: 1' \
-    --header 'Sec-GPC: 1' \
-    --header 'Connection: keep-alive' \
-    --header 'Referer: https://mwfa.dribl.com/' \
-    --header 'Sec-Fetch-Dest: empty' \
-    --header 'Sec-Fetch-Mode: cors' \
-    --header 'Sec-Fetch-Site: same-site' \
-    --header 'TE: trailers'
+curl 'https://mc-api.dribl.com/api/fixtures?date_range=default&season=3vmZpnGKq5&competition=R3NPZlPdr4&league=8nm7kPnLmO&club=RwNl4geNjr&tenant=kbam1QjmwX' \
+  --compressed \
+  -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:125.0) Gecko/20100101 Firefox/125.0' \
+  -H 'Accept: application/json' \
+  -H 'Accept-Language: en-US,en;q=0.5' \
+  -H 'Accept-Encoding: gzip, deflate' \
+  -H 'X-Requested-With: XMLHttpRequest' \
+  -H 'Origin: https://mwfa.dribl.com' \
+  -H 'DNT: 1' \
+  -H 'Sec-GPC: 1' \
+  -H 'Connection: keep-alive' \
+  -H 'Referer: https://mwfa.dribl.com/' \
+  -H 'Sec-Fetch-Dest: empty' \
+  -H 'Sec-Fetch-Mode: cors' \
+  -H 'Sec-Fetch-Site: same-site' \
+  -H 'TE: trailers'
